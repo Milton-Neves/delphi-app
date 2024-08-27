76152be2da7cf4794a6e240b33561375dbac9b66
Milton Bezerra das Neves Neto <m.neto@academico.ifrn.edu.br>
2024-08-27 08:13:37 -0300

    animation apply image

--- animation/Unit1.pas
+++ animation/Unit1.pas
@@ -5,12 +5,19 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Ani,
-  FMX.Objects;
+  FMX.Objects, FMX.Edit, FMX.StdCtrls, FMX.Controls.Presentation, FMX.Effects;
 
 type
   TForm1 = class(TForm)
     Image1: TImage;
     FloatAnimation1: TFloatAnimation;
+    FloatAnimation2: TFloatAnimation;
+    Label1: TLabel;
+    Button1: TButton;
+    Edit1: TEdit;
+    ShadowEffect1: TShadowEffect;
+    GlowEffect1: TGlowEffect;
+    ReflectionEffect1: TReflectionEffect;
   private
     { Private declarations }
   public
